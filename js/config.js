3d9344f54bee54e12690b5c71a0bac4ffd9195c2
student <iadjete1@jhu.edu>
2025-02-11 21:30:19 -0800

    Initial commit

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ window._config = {
     cognito: {
         userPoolId: 'us-west-1_H56RsNAHR', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '5ngouisv1ft1die36qiioi06h3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-1' // e.g. us-east-2
+        region: 'us-west-1'
     },
     api: {
-        invokeUrl: 'https://yn3ejj939j.execute-api.us-west-1.amazonaws.com/prod'
+        invokeUrl:'https://yn3ejj939j.execute-api.us-west-1.amazonaws.com/prod'
     }
 };
