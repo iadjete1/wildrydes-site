4616f800f52d4bf75ccda3893aeb07e1d1595cae
student <iadjete1@jhu.edu>
2025-02-11 17:54:50 -0800

    update js/config.JS

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-1_D1tustMru', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '37o8mt6ft02hv8tqe2o5l7o7j2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-1_H56RsNAHR', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5ngouisv1ft1die36qiioi06h3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-1' // e.g. us-east-2
     },
     api: {
